61785838e9f41ce07762325f401caccb08520179
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-22 17:35:20 +0200

    add Buefy, theme and font-awesome support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import Vue from "vue";
+import Buefy from "buefy";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@/theme/mingle-theme.scss";
 
 Vue.config.productionTip = false;
+library.add(fas);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.use(Buefy, { defaultIconPack: "fas" });
 
 new Vue({
   router,
