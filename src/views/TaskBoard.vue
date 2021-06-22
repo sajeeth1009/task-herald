2b2afc94179cf1a6bab1d196c025cc9a39bc799f
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-22 17:47:17 +0200

    remove about and create task board

---
+++ src/views/TaskBoard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="taskboard">
+    <h1>This is where you will see your tasks</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TaskBoard",
+  props: {},
+});
+</script>
