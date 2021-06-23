b384c40c554dbb4031c5b6a77f63bf45c34d3402
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-23 16:57:01 +0200

    Remove temp nav buttons

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <Navbar />
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/taskboard">My Taskboard</router-link>
-    </div>
     <router-view />
   </div>
 </template>
