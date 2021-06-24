3c4e878a66d962ffc9a6d7b20157e95a4a362d25
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-24 20:37:46 +0200

    remove unused API and add comments

--- src/api/taskAPI.ts
+++ src/api/taskAPI.ts
@@ -3,21 +3,35 @@ import { Task, TaskList } from "@/api/types/task";
 import { AxiosResponse } from "axios";
 
 // Task API's
+
+/**
+ * Get all Tasks
+ */
 export const getTaskListRequest = (): Promise<AxiosResponse<TaskList>> =>
   apiInstance.get<TaskList>("/v1/tasks");
 
-export const getTaskRequest = (taskId: string): Promise<AxiosResponse<Task>> =>
-  apiInstance.get<Task>(`/v1/tasks/${taskId}`);
-
+/**
+ * Create a new task
+ * @param task
+ */
 export const saveTaskRequest = (task: Task): Promise<AxiosResponse<string>> =>
-  apiInstance.post<string>("/v1/tasks", { task });
+  apiInstance.post<string>("/v1/tasks", task);
 
+/**
+ * Update Existing Task
+ * @param taskId
+ * @param task
+ */
 export const updateTaskRequest = (
   taskId: string,
   task: Partial<Task>
 ): Promise<AxiosResponse<Task>> =>
   apiInstance.patch<Task>(`/v1/tasks/${taskId}`, task);
 
+/**
+ * Delete a task by it's ID
+ * @param taskId
+ */
 export const deleteTaskRequest = (
   taskId: string
 ): Promise<AxiosResponse<Task>> =>
