ad8ad3ab8362a6716803f95dbb671ada19d7c2ef
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-24 20:37:24 +0200

    clean up store and promisify for toast alerts

--- src/store/modules/tasks.ts
+++ src/store/modules/tasks.ts
@@ -1,6 +1,11 @@
-import { Commit } from "vuex";
-import { Task } from "@/api/types/task";
-import { getTaskListRequest, updateTaskRequest } from "@/api/taskAPI";
+import { Commit, Dispatch } from "vuex";
+import { Task, TaskList } from "@/api/types/task";
+import {
+  getTaskListRequest,
+  updateTaskRequest,
+  saveTaskRequest,
+  deleteTaskRequest,
+} from "@/api/taskAPI";
 
 export interface TaskState {
   taskList: Task[];
@@ -9,10 +14,11 @@ export interface TaskState {
 }
 
 export interface CommitFunction {
+  dispatch: Dispatch;
   commit: Commit;
 }
 
-// initial state
+// INITIAL STATE
 const state = function (): TaskState {
   return {
     taskList: [] as Task[],
@@ -21,41 +27,106 @@ const state = function (): TaskState {
   };
 };
 
-// getters
-const getters = {};
-
-// actions
+// ACTIONS
 const actions = {
-  getTasks({ commit }: CommitFunction): void {
-    getTaskListRequest()
-      .then((response) => {
-        commit("setTasks", response.data);
-      })
-      .catch((err) => {
-        console.log("Failed to set Tasks", err);
-      });
+  /**
+   * Action to call the API to fetch all tasks from task-manager,
+   * and intialize store state.
+   *
+   * @param param0 Commit Function to call mutation set task
+   */
+  getTasks({ commit }: CommitFunction): Promise<TaskList> {
+    return new Promise((resolve, reject) => {
+      getTaskListRequest()
+        .then((response) => {
+          commit("setTasks", response.data);
+          resolve(response.data);
+        })
+        .catch((err) => {
+          console.log("Failed to set Tasks", err);
+          reject(err);
+        });
+    });
   },
+
+  /**
+   * Action to call update Task API
+   * @param param0 Commit Function to call mutation update task
+   * @param payload taskId, and task partial type
+   */
   updateTask(
     { commit }: CommitFunction,
     payload: { taskId: string; task: Partial<Task> }
-  ): void {
-    updateTaskRequest(payload.taskId, payload.task)
-      .then((response) => {
-        commit("updateTaskStore", response.data);
-      })
-      .catch((err) => {
-        console.log("Failed to update Task ", err);
-      });
+  ): Promise<Task> {
+    return new Promise((resolve, reject) => {
+      updateTaskRequest(payload.taskId, payload.task)
+        .then((response) => {
+          commit("updateTaskStore", response.data);
+          resolve(response.data);
+        })
+        .catch((err) => {
+          console.log("Failed to update Task ", err);
+          reject(err);
+        });
+    });
+  },
+
+  /**
+   * Action to call the create new Task API
+   * @param param0 Dispatch to trigger fresh collection of all tasks
+   * @param task Task details.
+   */
+  createTask({ dispatch }: CommitFunction, task: Task): Promise<string> {
+    return new Promise((resolve, reject) => {
+      saveTaskRequest(task)
+        .then((response) => {
+          dispatch("getTasks");
+          resolve(response.data);
+        })
+        .catch((err) => {
+          console.log("Failed to create Task ", err);
+          reject(err);
+        });
+    });
+  },
+
+  /**
+   * Action to call the delete Task API
+   * @param param0 Dispatch to trigger fresh collection of all tasks
+   * @param taskId Id of the task to be deleted
+   */
+  deleteTask({ dispatch }: CommitFunction, taskId: string): Promise<Task> {
+    return new Promise((resolve, reject) => {
+      deleteTaskRequest(taskId)
+        .then((response) => {
+          dispatch("getTasks");
+          resolve(response.data);
+        })
+        .catch((err) => {
+          console.log("Failed to create Task ", err);
+          reject(err);
+        });
+    });
   },
 };
 
-// mutations
+// MUTATIONS
 const mutations = {
+  /**
+   * Replace List of tasks in state, and update completed and pending tasks
+   * @param state
+   * @param taskResponse
+   */
   setTasks(state: TaskState, taskResponse: Task[]): void {
     state.taskList = taskResponse;
     updateTaskReport(state, taskResponse);
   },
 
+  /**
+   * Update A task in the state, and re-calculate completed & pending tasks
+   * @param state
+   * @param taskResponse
+   */
   updateTaskStore(state: TaskState, taskResponse: Task): void {
     state.taskList = state.taskList.map((task) => {
       return task.id === taskResponse.id ? taskResponse : task;
@@ -63,6 +134,11 @@ const mutations = {
     updateTaskReport(state);
   },
 
+  /**
+   * Remove a task from state, and re-calculate completed & pending tasks
+   * @param state
+   * @param taskId
+   */
   deleteTask(state: TaskState, taskId: string): void {
     state.taskList = state.taskList.filter((task) => {
       task.id === taskId;
@@ -74,10 +150,15 @@ const mutations = {
 export default {
   namespaced: true,
   state,
-  getters,
   actions,
   mutations,
 };
+
+/**
+ * Helper function to update state completed tasks and pending tasks
+ * @param state
+ * @param taskResponse
+ */
 function updateTaskReport(state: TaskState, taskResponse?: Task[]) {
   state.completedTasks = taskResponse
     ? taskResponse.filter((task) => task.completed).length
