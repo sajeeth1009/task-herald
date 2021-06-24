3375888ecfd7a41d587151bc8653a2f124a1eb26
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-24 20:40:00 +0200

    add images and layouts for landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <section>
+    <section class="hero is-halfheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-align-items-center">
+            <div class="column is-one-third-tablet">
+              <div class="title has-text-primary">Task Herald</div>
+              <div class="subtitle has-text-grey">
+                Your one-stop task organizer!
+              </div>
+            </div>
+            <div class="column two-one-third-tablet">
+              <carousel />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="hero has-background-grey-light is-medium">
+      <div class="hero-body">
+        <p class="title is-size-2">Get Started Today!</p>
+      </div>
+    </section>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/Landing.vue"; // @ is an alias to /src
+import Carousel from "@/components/Carousel.vue";
 
 export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld,
+    Carousel,
   },
 });
 </script>
