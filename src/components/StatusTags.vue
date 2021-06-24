c2dec9c1c77c27a7091a83e8c1bb9808920ce75d
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-24 20:35:40 +0200

    create separate component for status tags

---
+++ src/components/StatusTags.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-taglist>
+    <b-tag rounded class="is-success">
+      Your Completed Tasks: {{ completedTasks }}
+    </b-tag>
+    <b-tag rounded class="is-danger is-light">
+      Your Remaining Tasks: {{ pendingTasks }}
+    </b-tag>
+  </b-taglist>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    completedTasks: {
+      type: Number as () => number,
+    },
+    pendingTasks: {
+      type: Number as () => number,
+    },
+  },
+});
+</script>
