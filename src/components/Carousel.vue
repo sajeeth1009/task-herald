91df5b16fd48269d3cbc8e396bf42a58989919a1
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-24 20:35:04 +0200

    create carousel for landing page

---
+++ src/components/Carousel.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-carousel :indicator-inside="false">
+    <b-carousel-item v-for="(item, i) in 5" :key="i">
+      <b-image class="image" :src="getImgUrl(i)"></b-image>
+    </b-carousel-item>
+    <template #indicators="props">
+      <b-image
+        class="al image"
+        :src="getImgUrl(props.i)"
+        :title="props.i"
+      ></b-image>
+    </template>
+  </b-carousel>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  components: {},
+  methods: {
+    /**
+     * Method to fetch URL's while cycling through v-for index.
+     */
+    getImgUrl(value: number) {
+      return `/images/${value}-home.jpg`;
+    },
+  },
+});
+</script>
