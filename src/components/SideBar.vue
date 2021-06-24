50b2ff7dcc6862409cc1098271f86847ff039dc6
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-24 20:36:24 +0200

    create sidebar and textblock components

---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="column is-one-quarter-tablet">
+    <code>Your alerts</code>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Task } from "@/api/types/task";
+
+export default Vue.extend({
+  props: {
+    expiredTasks: {
+      type: Array as () => Task[],
+    },
+    timeAlertTasks: {
+      type: Array as () => Task[],
+    },
+  },
+});
+</script>
---
+++ src/components/TextBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="block has-text-left p-1" :class="backgroundType">
+    {{ textContent }}
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    textContent: {
+      type: String as () => string,
+    },
+    backgroundType: {
+      type: String as () => string,
+    },
+  },
+});
+</script>
